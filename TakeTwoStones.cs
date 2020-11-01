42d1d187a6cb5c150ebcf1dc4e45eba2d3b8caaf
Giorgos Leandros Kyriazis <leankyr@gmail.com>
2020-11-01 20:55:49 +0200

    Solved problem 2

---
+++ TakeTwoStones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KattisProblems
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            // give input 
+            do
+            {
+                Console.WriteLine("Enter Number Of Stones:");
+                int N = Console.ReadLine();
+            }
+            while(N < 1 || N Math.Pow(10, 7));
+
+
+        }
+    }
+}
---
+++ TakeTwoStones/TakeTwoStones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KattisProblems
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                long N = Int64.Parse(split[0]);
+
+                /// solve test case and output answer
+                if (N % 2 != 0)
+                {
+                    Console.WriteLine("Alice");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Bob");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
